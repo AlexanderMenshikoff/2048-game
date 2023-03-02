a9bf7cc9eaffbbdcfd1b0c9933b2f25f84ce324c
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-03-02 21:19:49 +0500

    added move up & move down functions

--- js/app.js
+++ js/app.js
@@ -79,12 +79,65 @@ document.addEventListener('DOMContentLoaded', ()=>{
         
     }
 
+
+    //свайп вниз 
+    function moveDown(){
+        for(let i = 0; i < 4; i++){
+             let totalOne = squares[i].innerHTML
+             let totalTwo = squares[i+width].innerHTML
+             let totalThree = squares[i+(width*2)].innerHTML
+             let totalFour = squares[i+(width*3)].innerHTML
+             let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+             let filteredColumn = column.filter(num=>num)
+             let missing = 4 - filteredColumn.length
+             let zeros = Array(missing).fill(0)
+             let newColumn = zeros.concat(filteredColumn)
+
+             squares[i].innerHTML = newColumn[0]
+             squares[i+width].innerHTML = newColumn[1]
+             squares[i+(width*2)].innerHTML = newColumn[2]
+             squares[i+(width*3)].innerHTML = newColumn[3]
+        }
+    }
+
+        //свайп вверх 
+        function moveUp(){
+            for(let i = 0; i < 4; i++){
+                 let totalOne = squares[i].innerHTML
+                 let totalTwo = squares[i+width].innerHTML
+                 let totalThree = squares[i+(width*2)].innerHTML
+                 let totalFour = squares[i+(width*3)].innerHTML
+                 let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+    
+                 let filteredColumn = column.filter(num=>num)
+                 let missing = 4 - filteredColumn.length
+                 let zeros = Array(missing).fill(0)
+                 let newColumn = filteredColumn.concat(zeros)
+    
+                 squares[i].innerHTML = newColumn[0]
+                 squares[i+width].innerHTML = newColumn[1]
+                 squares[i+(width*2)].innerHTML = newColumn[2]
+                 squares[i+(width*3)].innerHTML = newColumn[3]
+            }
+        }
+
     function combineRow(){
         for(let i = 0; i < 15; i++){
             if(squares[i].innerHTML === squares[i+1].innerHTML){
                 let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
                 squares[i].innerHTML = combinedTotal
                 squares[i+1].innerHTML = 0
+            }
+        }
+    }
+
+    function combineColumn(){
+        for(let i = 0; i < 12; i++){
+            if(squares[i].innerHTML === squares[i+width].innerHTML){
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+width].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i+width].innerHTML = 0
             }
         }
     }
@@ -97,6 +150,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         }else if(e.keyCode === 37){
             keyLeft()
+        }else if(e.keyCode === 38){
+            keyUp()
+        }else if(e.keyCode === 40){
+            keyDown()
         }
     }
 
@@ -113,6 +170,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
         moveLeft()
         combineRow()
         moveLeft()
+        generate()
+    }
+
+    function keyDown(){
+        moveDown()
+        combineColumn()
+        moveDown()
+        generate()
+    }
+
+    function keyUp(){
+        moveUp()
+        combineColumn()
+        moveUp()
         generate()
     }
 
